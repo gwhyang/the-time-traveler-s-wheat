c2c4fca8c89ac798dd83b9801a9c63bfec6ed1fd
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-09-12 16:56:54 +0800

    1

--- Global/game.gd
+++ Global/game.gd
@@ -1,7 +1,8 @@
 extends Node
 enum GameMode{WALK,DIALOGUE}
+enum BackGround{NONE,HOME,STREET,CLASSROOM,CORRIDOR,LAKE,LAB}
 var game_mode:GameMode=GameMode.WALK
-
+var scene_flag:int = 1
 func _ready() -> void:
 	Dialogic.timeline_started.connect(on_timeline_start)
 	Dialogic.timeline_ended.connect(on_timeline_end)
