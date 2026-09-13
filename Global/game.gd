01f10de0f24fc57e3fb37c1caad91cbbfd456ae3
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-09-13 13:43:21 +0800

    完善对话演出

--- Global/game.gd
+++ Global/game.gd
@@ -3,6 +3,9 @@ enum GameMode{WALK,DIALOGUE}
 enum BackGround{NONE,HOME,STREET,CLASSROOM,CORRIDOR,LAKE,LAB}
 var game_mode:GameMode=GameMode.WALK
 var scene_flag:int = 1
+
+signal background_changed(id:int)
+
 func _ready() -> void:
 	Dialogic.timeline_started.connect(on_timeline_start)
 	Dialogic.timeline_ended.connect(on_timeline_end)
@@ -12,3 +15,6 @@ func on_timeline_start():
 	
 func on_timeline_end():
 	game_mode = GameMode.WALK
+
+func change_back_ground(id:int):
+	background_changed.emit(id)
